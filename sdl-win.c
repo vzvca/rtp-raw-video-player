e707872631c335fb73ad327c512d5c1d0d7ea22f
vca <vca@somewhere.com>
2020-12-25 08:53:56 +0100

    add parameters to sdl-win

--- sdl-win.c
+++ sdl-win.c
@@ -30,11 +30,20 @@
 #include <sys/ioctl.h>
 
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 #define WIDTH 720
 #define HEIGHT 576
-#define IMG_PATH "deco-salon.png"
+#define FPS 10
+#define WINX 100
+#define WINY 100
+
+int g_x = WINX;
+int g_y = WINY;
+int g_width = WIDTH;
+int g_height = HEIGHT;
+int g_fps = FPS;
+char *g_input = NULL;
+
 
 int SDLCALL
 ThreadFunc(void *data)
@@ -44,56 +53,45 @@ ThreadFunc(void *data)
   SDL_Window *win = NULL;
   SDL_Renderer *renderer = NULL;
   SDL_Texture *img = NULL;
-  int w = WIDTH, h = HEIGHT; // texture width & height
   char *pixels = NULL;
   int fbfd;
   
   // create the window and renderer
   // note that the renderer is accelerated
   puts("create window");
-  win = SDL_CreateWindow("video capture", 100, 100, WIDTH, HEIGHT, 0);
+  win = SDL_CreateWindow("video capture", g_x, g_y, g_width, g_height, 0);
 
   puts("create renderer");
   renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
   puts("create texture");
   img = SDL_CreateTexture(renderer,
-			  SDL_PIXELFORMAT_ARGB32, SDL_TEXTUREACCESS_STREAMING,
-			  w, h);
+			  SDL_PIXELFORMAT_BGRA32, SDL_TEXTUREACCESS_STREAMING,
+			  g_width, g_height);
+  // Display window
+  SDL_ShowWindow(win);
 
   puts("mmap video source");
-  fbfd = open( "toto.img", O_RDWR );
+  fbfd = open( g_input, O_RDWR );
   if (fbfd == -1) {
     perror("Error: cannot open output file");
     exit(1);
   }
-  printf("The output file was opened successfully.\n");
-  /* Map the device to memory */
-  pixels = (char *)mmap(0, w*h*4, PROT_READ, MAP_SHARED, fbfd, 0);
+  puts("The output file was opened successfully.");
+  pixels = (char *)mmap(0, g_width*g_height*4, PROT_READ, MAP_SHARED, fbfd, 0);
   if (*(int*)pixels == -1) {
     perror("Error: failed to map output file to memory");
-    exit(4);
+    exit(1);
   }
-  printf("The output file was mapped to memory successfully.\n");
-  
-  
-  // load our image
-  //  img = IMG_LoadTexture(renderer, IMG_PATH);
-  //  SDL_QueryTexture(img, NULL, NULL, &w, &h); // get the width and height of the texture
-  // put the location where we want the texture to be drawn into a rectangle
-  // I'm also scaling the texture 2x simply by setting the width and height
-  //  SDL_Rect texr; texr.x = WIDTH/2; texr.y = HEIGHT/2; texr.w = w*2; texr.h = h*2; 
-SDL_Rect texr; texr.x = 0; texr.y = 0; texr.w = WIDTH; texr.h = HEIGHT; 
-  puts("entering main loop");
+  puts("The output file was mapped to memory successfully.\n");
   
   
   // main loop
+  puts("entering main loop");
   while (1) {
-    puts("looping");
-		
     // event handling
     SDL_Event e;
-    if ( SDL_WaitEventTimeout(&e,1000) ) {
+    if ( SDL_WaitEventTimeout(&e,1000/g_fps) ) {
       if (e.type == SDL_QUIT)
 	break;
       else if (e.type == SDL_KEYUP && e.key.keysym.sym == SDLK_ESCAPE)
@@ -104,32 +102,72 @@ SDL_Rect texr; texr.x = 0; texr.y = 0; texr.w = WIDTH; texr.h = HEIGHT;
     SDL_RenderClear(renderer);
 
     // update video frame
-    SDL_UpdateTexture( img, NULL, pixels, w*4 );
+    SDL_UpdateTexture( img, NULL, pixels, g_width*4 );
     
     // copy the texture to the rendering context
     SDL_RenderCopy(renderer, img, NULL, NULL);
+    
     // flip the backbuffer
     // this means that everything that we prepared behind the screens is actually shown
     SDL_RenderPresent(renderer);
-    SDL_ShowWindow(win);
-		
   }
 	
-  //SDL_DestroyTexture(img);
+  SDL_DestroyTexture(img);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(win);
-	
+  munmap( pixels, g_width*g_height*4 );
+  close(fbfd);
+  
   return 0;
 }
 
+/*
+ * --------------------------------------------------------------------------
+ *   Usage
+ * --------------------------------------------------------------------------
+ */
+void usage( int argc, char *argv[], int optind )
+{
+  char *what = (optind > 0) ? "error" : "usage";
+  fprintf( stderr, "%s %s :\n%s [-?] -i file [-f fps] [-x x] [-y y] [-w width] [-h height]\n",
+	   argv[0], what, argv[0]);
+  
+  fprintf( stderr, "\t-?\t\tPrints this message.\n");
+  fprintf( stderr, "\t-i\t\tSets input video frame file.\n");
+  fprintf( stderr, "\t-f\t\tSets the video framerate (default %d).\n", FPS);
+  fprintf( stderr, "\t-w\t\tSets the width of the image (default %d).\n", WIDTH);
+  fprintf( stderr, "\t-h\t\tSets the height of the image (default %d).\n", HEIGHT);
+  fprintf( stderr, "\t-x\t\tSets the x position of the window (default %d).\n", WINX);
+  fprintf( stderr, "\t-y\t\tSets the y position of the window (default %d).\n", WINY);
+  /* exit with error only if option parsng failed */
+  exit(optind > 0);
+}
+
+// --------------------------------------------------------------------------
+//   Programme principal
+// --------------------------------------------------------------------------
 int main (int argc, char *argv[])
 {
   SDL_Thread *thread;
-
-  puts("init sdl");
+  int opt;
+  
+  while ( (opt = getopt( argc, argv, "?i:f:w:h:x:y:")) != -1 ) {
+    switch( opt ) {
+    case '?':  usage( argc, argv, 0); break;
+    case 'i':  g_input = optarg; break;
+    case 'w':  g_width = atoi(optarg); break;
+    case 'h':  g_height = atoi(optarg); break;
+    case 'f':  g_fps = atoi(optarg); break;
+    case 'x':  g_x = atoi(optarg); break;
+    case 'y':  g_y = atoi(optarg); break;
+    default:
+      usage(argc, argv, optind);
+    }
+  }
+  
   
   // Initialize SDL.
-  //  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+  puts("init sdl");
   if (SDL_Init(0) < 0) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Init failed: %s\n", SDL_GetError());
     return 1;
@@ -139,8 +177,6 @@ int main (int argc, char *argv[])
     return 1;
   }
 
-  puts("init done");
-  
   thread = SDL_CreateThread(ThreadFunc, "One", "#1");
   if (thread == NULL) {
     SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create thread: %s\n", SDL_GetError());
